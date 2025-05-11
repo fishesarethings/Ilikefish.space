857fa724086c90ae63ebe32c7fe351630151d1b9
Ca <65434532+fishesarethings@users.noreply.github.com>
2025-05-11 19:57:43 +0800

    Create typing.js

---
+++ assets/JS/typing.js
@@ -0,0 +1,22 @@
+const text = 'I like fishes ';
+const cursor = '_';
+let i = 0;
+
+function type() {
+  if (i < text.length) {
+    document.getElementById('hero-title').textContent += text.charAt(i++);
+    setTimeout(type, 150);
+  } else {
+    blink();
+  }
+}
+
+function blink() {
+  let show = true;
+  setInterval(() => {
+    document.getElementById('hero-title').textContent = text + (show ? cursor : '');
+    show = !show;
+  }, 500);
+}
+
+window.addEventListener('load', type);
