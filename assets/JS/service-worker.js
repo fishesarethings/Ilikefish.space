17af4420318ee716a875342d29c6784ff8c4114e
Ca <65434532+fishesarethings@users.noreply.github.com>
2025-05-11 20:41:41 +0800

    Update service-worker.js

--- assets/JS/service-worker.js
+++ assets/JS/service-worker.js
@@ -1,23 +1,9 @@
-const CACHE = 'fish-site-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/games.html',
-  '/server.html',
-  '/assets/css/styles.css',
-  '/assets/js/loco.js',
-  '/assets/js/typing.js',
-  '/assets/js/sw-register.js',
-  '/assets/js/server.js',
-  '/assets/js/games-list.js',
-  '/manifest.json'
-  // plus each game folder & its assets
+const CACHE='fish-site-v2';
+const ASSETS=[
+  '/', '/index.html','/games.html','/server.html',
+  '/assets/css/styles.css','/assets/js/typing.js','/assets/js/loco.js',
+  '/assets/js/games-list.js','/assets/js/sw-register.js','/assets/js/service-worker.js',
+  '/assets/js/server.js','/assets/js/fullscreen.js','/manifest.json'
 ];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
