54621c2489e57a89a56c80a0924b6c2779ca9cfe
Ca <65434532+fishesarethings@users.noreply.github.com>
2025-05-19 17:50:45 +0800

    update

--- scripts/gen-manifest.js
+++ scripts/gen-manifest.js
@@ -1,11 +1,15 @@
 // scripts/gen-manifest.js
-import fs from 'fs';
-import glob from 'glob';
-import path from 'path';
+const fs   = require('fs');                              // :contentReference[oaicite:0]{index=0}
+const path = require('path');
+const glob = require('glob');
 
-const files = glob.sync('**/*.*', { ignore: ['node_modules/**', '.git/**'] });
-const manifest = files.map(f => '/' + f.replace(/\\/g, '/'));
+// Crawl this directory (project root)
+const files = glob.sync('**/*.*', {
+  ignore: ['node_modules/**', '.git/**']
+});
+
+const manifest = files.map(f => '/' + f.replace(/\\/g,'/'));
 const content  = `self.__WB_MANIFEST = ${JSON.stringify(manifest)};`;
 
-fs.writeFileSync(path.resolve('precache-manifest.js'), content);
+fs.writeFileSync(path.join(__dirname, '../precache-manifest.js'), content);
 console.log('precache-manifest.js generated with', manifest.length, 'entries');
